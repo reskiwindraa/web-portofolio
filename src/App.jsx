1b2273871a4aca3a4bd77309a51a7c574246b7d4
Reski Windradiaksa <reskiwindra004@gmail.com>
2025-12-04 16:43:59 +0700

    bio profil

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
-import { useState } from 'react'
-
-
+import DataImage from "./data";
+import { useState } from "react";
+import { HiArrowDownTray, HiEnvelope } from "react-icons/hi2";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <>
-    {/* <h1 className='text-4xl font-bold'>Halo Dunia</h1> */}
+      <div className="hero grid grid-cols-2">
+        <div>
+          <div className="flex items-center gap-3 mb-6 w-fit p-1 hover:scale-105 transition-all hover:bg-violet-900 rounded-[10px]">
+            <div className="flex items-center bg-zinc-800 p-4 rounded-lg">
+              <img
+                src={DataImage.HeroImage}
+                alt="Hero Image"
+                className="w-10 rounded-md"
+              />
+              <q className="text-white ml-4 ">
+                Kode yang indah, lahir dari ketekunan.:)
+              </q>
+            </div>
+          </div>
+
+          <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Jhon Thor</h1>
+          <p className="text-base/loose font-mono opacity-70">
+            Seorang UI/UX dan Frontend Developer dengan fokus pada pengembangan
+            antarmuka mobile banking yang modern, intuitif, dan berorientasi
+            pada pengalaman pengguna. Berpengalaman dalam membuat ilustrasi
+            interface, mengembangkan prototype, serta melakukan penyempurnaan
+            alur dan tampilan aplikasi agar lebih efisien dan mudah digunakan.
+            Memiliki ketertarikan pada desain sistem, konsistensi visual, dan
+            peningkatan kualitas produk melalui detail-detail kecil yang
+            berdampak besar.
+          </p>
+          <div className="flex items-center gap-4 text-center">
+            <a
+              href="#"
+              className="flex items-center gap-2 pl-6 pr-6 bg-violet-900 p-3 rounded-full hover:scale-102 transition hover:bg-violet-700 duration-200 erase-in-out transform"
+            >
+              <span>Download CV</span>
+              <HiArrowDownTray />
+            </a>
+            <a
+              href="#"
+              className="flex items-center gap-2 pl-6 pr-6 border-2 border-violet-900 p-3 rounded-full hover:scale-102 transition hover:border-violet-700 duration-200 erase-in-out transform"
+            >
+              <span>Contact</span>
+              <HiEnvelope />
+            </a>
+          </div>
+        </div>
+        <img
+          src={DataImage.HeroImage}
+          alt="Hero Image"
+          className="w-[500px] ml-auto"
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,57 @@
+import { useState,useEffect, use } from 'react'
 import React from 'react'
 
 const Navbar = () => {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 150) {
+                setActive(true);
+                } else {
+                setActive(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    },[]);
+
   return (
     <div className='navbar py-7 flex items-center justify-between px-6'>
         <div className='logo'>
-        <h1 className='text-3xl font-bold'>Portofolio</h1>
+        <h1 className='text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white'>Portofolio</h1>
         </div>
 
-        <ul className='menu flex items-center gap-10 sm:static text-lg font-medium fixed left-1/2 -translate-x-1/2 
-        md:translate-x-0'>
+        <ul className={`menu flex items-center sm:gap-10 gap-4 md:static text-lg font-medium fixed left-1/2 -translate-x-1/2 
+        md:translate-x-0 md:opacity-100 bg-white/30 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent 
+        transition-all md-transition-none ${
+            active ? "top-0 opacity-100" : "-top-10 opacity-0"
+            }`}>
             <li>
-                <a href="#" className='text-lg font-medium hover:font-semibold'>Beranda</a>
+                <a href="#" className='sm:text-lg text-base font-medium hover:font-semibold transition-all duration-200'>
+                    Beranda</a>
             </li>
             <li>
-                <a href="#" className='text-lg font-medium hover:font-semibold'>Tentang</a>
+                <a href="#" className='sm:text-lg text-base font-medium hover:font-semibold transition-all'>
+                    Tentang</a>
             </li>
             <li>
-                <a href="#" className='text-lg font-medium hover:font-semibold'>Projek</a>
+                <a href="#" className='sm:text-lg text-base font-medium hover:font-semibold transition-all'>
+                    Projek</a>
             </li>
             <li>
-                <a href="#" className='text-lg font-medium hover:font-semibold'>Kontak</a>
+                <a href="#" className='sm:text-lg text-base font-medium hover:font-semibold transition-all'>
+                    Kontak</a>
             </li>
         </ul>
     </div>
     
+    
+    
   )
+  return
 }
 
 export default Navbar
